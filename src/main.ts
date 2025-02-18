0f8bc9424fa5fdfc84133f47c6fad0f36eb96424
dominik.haas <dominik.haas@qaware.de>
2025-02-18 11:54:23 +0100

    Fix import for i18n

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from './App.vue'
 import './index.css'
 import { createPinia } from 'pinia'
 import router from './router'
+import i18n from '@/i18n/i18n.ts'
 
-import i18n from '../i18n/i18n.ts'
+
 
 createApp(App).use(router).use(createPinia()).use(i18n).mount('#app')
